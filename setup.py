0e67c65510ebc70b7d6b5ed35baf174a796513d6
Jason Goodell <jgoodell77@gmail.com>
2014-12-19 15:49:17 -0500

    Getting ready to deploy.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='dyneav',
-      version='1.0-beta',
+      version='1.0',
       description='A little EAV package that lets you create dynamic classes using EAV to store attributes.',
       author='Jason Goodell',
       author_email='jgoodell77@gmail.com',
